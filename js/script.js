d708b9a5b96c5b33605dac1b6e3bc60ca93677a7
wbz0100 <91980289+wbz0100@users.noreply.github.com>
2023-08-21 22:25:18 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,12 +3,13 @@ const userData = document.getElementById("userData")
 const worldInfo = document.getElementById("worldInfo")
 
 function catchLogs(data) {
+  const logLine = data.line
 
-  switch (data.line[0]) {
+  switch (logLine[0]) {
     case '01': {
       const type = "지역 변경"
-      const terrID = data.line[2]
-      const terrName = data.line[3]
+      const terrID = logLine[2]
+      const terrName = logLine[3]
 
       terrInfo.innerHTML = `${terrName} (${terrID})`
 
@@ -17,8 +18,8 @@ function catchLogs(data) {
     break
     case '02': {
       const type = "플레이어 정보 갱신"
-      const id = data.line[2]
-      const name = data.line[3]
+      const id = logLine[2]
+      const name = logLine[3]
 
       userData.innerHTML = `${name} (${id})`
 
@@ -26,39 +27,39 @@ function catchLogs(data) {
     }
     case '03': {
       const type = "새 객체 정보"
-      const id = data.line[2] 
-      const name = data.line[3]
-      const npcNameID = data.line[9]
-      const currHP = data.line[11]
-      const maxHP = data.line[12]
-      const logX = data.line[17]
-      const logY = data.line[18]
-      const logZ = data.line[19]
-      const radius = data.line[20]
+      const id = logLine[2] 
+      const name = logLine[3]
+      const npcNameID = logLine[9]
+      const currHP = logLine[11]
+      const maxHP = logLine[12]
+      const logX = logLine[17]
+      const logY = logLine[18]
+      const logZ = logLine[19]
+      const radius = logLine[20]
       return
     }
     break
     case '04': {
       const type = "객체 정보 삭제"
-      const id = data.line[2]
-      const name = data.line[3]
-      const npcNameID = data.line[9]
-      const currHP = data.line[11]
-      const maxHP = data.line[12]
-      const logX = data.line[17]
-      const logY = data.line[18]
-      const logZ = data.line[19]
-      const radius = data.line[20]
+      const id = logLine[2]
+      const name = logLine[3]
+      const npcNameID = logLine[9]
+      const currHP = logLine[11]
+      const maxHP = logLine[12]
+      const logX = logLine[17]
+      const logY = logLine[18]
+      const logZ = logLine[19]
+      const radius = logLine[20]
       return
     }
     break
     case '261': {
       const type = "메모리 객체 값"
-      const status = data.line[2]
-      const id = data.line[3]
-      const currWorldID = data[data.indexOf('CurrentWorldID') + 1]
-      const name = data[data.indexOf('Name') + 1]
-      const bnpcNameID = data[data.indexOf('BnpcNameID') + 1]
+      const status = logLine[2]
+      const id = logLine[3]
+      const currWorldID = logLine[logLine.indexOf('CurrentWorldID') + 1]
+      const name = logLine[logLineindexOf('Name') + 1]
+      const bnpcNameID = logLine[logLine.indexOf('BnpcNameID') + 1]
 
       if (status === 'Add' && name === '백측' && id === '10135A95') {
         userData.innerHTML = `${name} (${id})`
