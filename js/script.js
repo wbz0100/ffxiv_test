d836d947dad2cc822ee9bb9992a85b4f6dc6c952
wbz0100 <91980289+wbz0100@users.noreply.github.com>
2023-08-21 17:26:00 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 function catchLogs(data) {
   console.log (data.line)
+
+  try {
+    switch (data.line[0]) {
+      "01": {
+        const type = "지역 변경"
+        const terrID = data.line[3]
+        const terrName = data.line[4]
+      }
+      console.log (`${type} : ${terrName} (${terrID})`)
+      break
+      default:
+      break
+    }
+  }catch (error)
+  console.log (error)
+  throw error
 }
 
 addOverlayListener('LogLine', catchLogs); // 등록
