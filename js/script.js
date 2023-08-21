75414878bcad87a04ad6eeaa082190b1d9c5026e
wbz0100 <91980289+wbz0100@users.noreply.github.com>
2023-08-21 18:44:02 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,58 @@
 function catchLogs(data) {
-  console.log (data.line)
 
-  try {
-    switch (data.line[0]) {
-      case '01': {
-        const type = "지역 변경"
-        const terrID = data.line[3]
-        const terrName = data.line[4]
+  switch (data.line[0]) {
+    case '01': {
+      const type = "지역 변경"
+      const terrID = data.line[3]
+      const terrName = data.line[4]
 
-        console.log (`${type} : ${terrName} (${terrID})`)
-      }
-      break
-      case '02': {
-        const type = "플레이어 정보 갱신"
-        const id = data.line[2]
-        const name = data.line[3]
-
-        console.log (`${type} : ${name} (${id})`)
-      }
-      break
-      default: return
+      console.log (`${type} : ${terrName} (${terrID})`)
     }
-  }catch (error)
-  console.log (error)
-  throw error
+    break
+    case '02': {
+      const type = "플레이어 정보 갱신"
+      const id = data.line[2]
+      const name = data.line[3]
+
+      console.log (`${type} : ${name} (${id})`)
+    }
+    case '03': {
+      const type = "새 객체 정보"
+      const id = data.line[2]
+      const name = data.line[3]
+      const npcNameID = data.line[9]
+      const currHP = data.line[11]
+      const maxHP = data.line[12]
+      const logX = data.line[17]
+      const logY = data.line[18]
+      const logZ = data.line[19]
+      const radius = data.line[20] 
+
+      const currHPPer = maxHP/currHP * 100
+
+      console.log (`${type} : ${name} (${id}) HP: ${currHP}/${maxHP} (${currHPPer}%)`)
+    }
+    break
+    case '04': {
+      const type = "객체 정보 삭제"
+      const id = data.line[2]
+      const name = data.line[3]
+      const npcNameID = data.line[9]
+      const currHP = data.line[11]
+      const maxHP = data.line[12]
+      const logX = data.line[17]
+      const logY = data.line[18]
+      const logZ = data.line[19]
+      const radius = data.line[20] 
+
+      const currHPPer = maxHP/currHP * 100
+
+      console.log (`${type} : ${name} (${id}) HP: ${currHP}/${maxHP} (${currHPPer}%)`)
+    }
+    break
+    default: return
+  }
+
 }
 
 addOverlayListener('LogLine', catchLogs); // 등록
